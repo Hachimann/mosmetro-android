db336b812409a21a19c075a384009f2f7644c9bf
Shandor Hachimann <shandor34@gmail.com>
2020-11-25 03:35:24 +0300

    Migrated to AndroidX

--- app/src/main/java/pw/thedrhax/mosmetro/activities/SettingsActivity.java
+++ app/src/main/java/pw/thedrhax/mosmetro/activities/SettingsActivity.java
@@ -40,9 +40,6 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -50,6 +47,10 @@ import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import org.acra.ACRA;
 
